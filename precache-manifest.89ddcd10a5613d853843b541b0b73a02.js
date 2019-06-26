dc9cecc8d85ea960b838758fd4a635b53d47a74d
SHE\craig.gall <craig.gall@shesoftware.com>
2019-06-26 09:33:47 +0100

    Test site for image compression

---
+++ precache-manifest.89ddcd10a5613d853843b541b0b73a02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "986c56c68b5deb5a1d40acf460c7159e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6a22899b17c7324d85d",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "4bd7eb1ab7a52408b322",
+    "url": "/static/js/2.917b67d6.chunk.js"
+  },
+  {
+    "revision": "b6a22899b17c7324d85d",
+    "url": "/static/js/main.ba36bc2b.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
